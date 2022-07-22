2a605ea10a653683949ee17145b99bda14408831
LorisP96 <lorispalazzo96@gmail.com>
2022-07-22 17:37:13 +0200

    snack 3

---
+++ Snack-3/index.php
@@ -0,0 +1,30 @@
+## Snack 3
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack3</title>
+</head>
+<body>
+    <?php 
+        $myArray = [];
+
+        while(count($myArray) < 15) {
+            $randomNum = rand(1, 50);
+            if(!in_array($randomNum, $myArray)) {
+                $myArray[] = $randomNum;
+            };  
+        };
+    ?>
+    <h1>
+        <?php for($i = 0; $i < count($myArray); $i++) { 
+            $myNum += $myArray[$i];
+            echo $myNum . ',';
+        } ?>
+    </h1>
+</body>
+</html>
